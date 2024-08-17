22f6303bbf0d4057c0ab6c1ab4e9098906e7c16c
Joshua Terry <giodev1999@gmail.com>
2024-08-17 14:07:08 +0100

    Initial Commit

---
+++ src/client/java/net/devgio/passive_enchantments/client/PassiveEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package net.devgio.passive_enchantments.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PassiveEnchantmentsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/devgio/passive_enchantments/PassiveEnchantments.java
@@ -0,0 +1,15 @@
+package net.devgio.passive_enchantments;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PassiveEnchantments implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("Passive Enchantments");
+    public static final String MOD_ID = "passive_enchantments";
+
+    @Override
+    public void onInitialize() {
+    }
+}
