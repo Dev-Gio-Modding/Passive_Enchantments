6b0e6d925346c75cd967e54c82c1726be6d482eb
Joshua Terry <giodev1999@gmail.com>
2024-08-25 20:39:34 +0100

    Refactor Hoglin behavior and add BrainMixin

--- src/main/java/net/devgio/passive_enchantments/mixin/passive/BrainMixin.java
+++ src/main/java/net/devgio/passive_enchantments/mixin/passive/BrainMixin.java
@@ -3,34 +3,37 @@ package net.devgio.passive_enchantments.mixin.passive;
 import net.devgio.passive_enchantments.Logger;
 import net.devgio.passive_enchantments.enchantments.Enchantments;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.Brain;
+import net.minecraft.entity.ai.brain.Memory;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.mob.HoglinEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+//doesnt work
+
+@Mixin(Brain.class)
+public abstract class BrainMixin {
+    @Inject(method = "setMemory", at = @At("HEAD"), cancellable = true)
+    private <U> void setMemoryInternal(MemoryModuleType<U> moduleType, Optional<? extends Memory<?>> memory, CallbackInfo callback) {
+
+        EntityType<?> type = ((MobEntity) (Object) this).getType();
+
+        if (type == EntityType.HOGLIN) {
+
+            HoglinEntity hoglin = (HoglinEntity) (Object) this;
+            LivingEntity target = hoglin.getTarget();
 
 
-@Mixin(HoglinEntity.class)
-public abstract class HoglinMixin {
-
-
-    @Inject(method = "tryAttack",at = @At("HEAD"), cancellable = true)
-    private void onAttacking(Entity target, CallbackInfoReturnable<Boolean> cir)
-    {
-
-        Logger.LOGGER.info("test");
-
-        HoglinEntity hoglin = (HoglinEntity) (Object) this;
-        Brain<HoglinEntity> brain = hoglin.getBrain();
-
-
-
-        if (target == null){
+            if (target == null){
                 return;
             }
             if (target.isPlayer()){
@@ -46,16 +49,12 @@ public abstract class HoglinMixin {
                     if (EnchantmentHelper.hasAnyEnchantmentsIn(stack, Enchantments.PASSIVE_HOGLIN)) {
 
                         Logger.LOGGER.info("test cancel");
-                        brain.forget(MemoryModuleType.ATTACK_TARGET);
-
-
-                        cir.setReturnValue(false);
-
-
-
-
+                        if (moduleType == MemoryModuleType.ATTACK_TARGET)
+                            callback.cancel();
                     }
                 }
             }
         }
+
     }
+}
