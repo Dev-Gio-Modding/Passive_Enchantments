04f93273cd3952e80f609c7c2d1c9e59ec6afc9f
Joshua Terry <giodev1999@gmail.com>
2024-09-05 11:08:49 +0100

    Revert "remove disabled mixins"
    This reverts commit f2f45d57408b18fe493840e6a80a0573ccef5f1d.

---
+++ src/main/java/net/devgio/passive_enchantments/mixin/disabled/PhantomMixin.java
@@ -0,0 +1,48 @@
+    package net.devgio.passive_enchantments.mixin.disabled;
+
+    import net.devgio.passive_enchantments.enchantments.Enchantments;
+    import net.minecraft.enchantment.EnchantmentHelper;
+    import net.minecraft.entity.EntityType;
+    import net.minecraft.entity.LivingEntity;
+    import net.minecraft.entity.mob.*;
+    import net.minecraft.item.ItemStack;
+    import org.jetbrains.annotations.Nullable;
+    import org.spongepowered.asm.mixin.Mixin;
+    import org.spongepowered.asm.mixin.Shadow;
+    import org.spongepowered.asm.mixin.injection.At;
+    import org.spongepowered.asm.mixin.injection.Inject;
+    import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+    @Mixin(MobEntity.class)
+    abstract
+    class PhantomMixin {
+
+        @Shadow private @Nullable LivingEntity target;
+
+        @Inject(method = "getTarget", at = @At("HEAD"), cancellable = true)
+        private void getTarget(CallbackInfoReturnable<Boolean> cir) {
+
+            if (EntityType.PHANTOM == ((MobEntity)(Object)this).getType()) {
+
+                PhantomEntity mob = (PhantomEntity) (Object) this;
+                LivingEntity target = this.target;
+
+                if (target == null){
+                    return;
+                }
+
+                if (target.isPlayer()){
+                    if (mob.getAttacker() == target){
+                        return;
+                    }
+                    for (ItemStack stack : target.getArmorItems()) {
+
+                        if (EnchantmentHelper.hasAnyEnchantmentsIn(stack, Enchantments.PASSIVE_PHANTOM)) {
+                            cir.setReturnValue(null);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
---
+++ src/main/java/net/devgio/passive_enchantments/mixin/disabled/SilverfishMixin.java
@@ -0,0 +1,50 @@
+package net.devgio.passive_enchantments.mixin.disabled;
+
+import net.devgio.passive_enchantments.enchantments.Enchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.*;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+abstract class SilverfishMixin {
+
+    @Shadow private @Nullable LivingEntity target;
+
+    @Inject(method = "getTarget", at = @At("HEAD"), cancellable = true)
+    private void getTarget(CallbackInfoReturnable<Boolean> cir) {
+
+        EntityType<?> type = ((MobEntity)(Object)this).getType();
+        
+        if (type == EntityType.SILVERFISH) {
+
+            SilverfishEntity mob = (SilverfishEntity) (Object) this;
+            LivingEntity target = this.target;
+
+            if (target == null) {
+                return;
+            }
+
+            if (target.isPlayer()) {
+                if (mob.getAttacker() == target) {
+                    return;
+                }
+                for (ItemStack stack : target.getArmorItems()) {
+
+                    if (EnchantmentHelper.hasAnyEnchantmentsIn(stack, Enchantments.PASSIVE_SILVERFISH)) {
+                        cir.setReturnValue(null);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
